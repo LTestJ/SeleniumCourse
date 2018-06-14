d0350f2f429ee4b78dd9bfc218d116418f5eb564
Jose Devassy <jose123go@gmail.com>
2018-06-13 21:01:11 -0700

    adding two files arrays Signed-off-by: Jose Devassy <jose123go@gmail.com>

--- Examples/src/ArraySum.java
+++ Examples/src/ArraySum.java
@@ -4,15 +4,15 @@ public class ArraySum {
 	public static void main(String[] args) {
 		 int[] numbers={1,2,3,4,5};
 		 System.out.println(Sum(numbers));
-	}
+		 }
 	
 	//prototype of function
 	static int Sum(int[] numbers){
-		
+		//double average=1;
 		int sum=0;
 		for(int i=0; i<numbers.length; i++) {
-			sum=sum+numbers[i];
+			sum=sum+numbers[i];	
 		}
-		return sum;
+		return sum;		
 	}
 }
