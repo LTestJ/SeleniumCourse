1d10d136d0f108c3c56be5ed509e17996c91b326
Jose Devassy <jose123go@gmail.com>
2018-06-15 22:12:54 -0700

    Signed-off-by: Jose Devassy <jose123go@gmail.com>

--- Examples/src/Average.java
+++ Examples/src/Average.java
@@ -1,20 +1,18 @@
 
 public class Average {
 
-	public static void main(String[] args){
-		
-		double[] numbers={6,1,2,4,5,6,7,1};
-		 System.out.println("Sum is "+  Sum(numbers));
+	public static void main(String[] args) {		
+		double[] numbers = {4,1,2,4,5,6,7,1};
+		System.out.println("average is " +  average(numbers));
+	}
+	
+	static double average(double[] numbers) {		
+		double sum = 0;
+		for(int i = 0; i < numbers.length; i++) {
+			sum = sum + numbers[i];	
 		}
 	
-	static double Sum(double[] numbers){		
-		double sum=0;
-		for(int i=0; i<numbers.length; i++) {
-			sum=sum+numbers[i];	
-		}
-		
-		double average = sum/numbers.length;
-		System.out.println("average is " + average);
-		return sum;
-		}
+		double average = sum/numbers.length;		
+		return average;
+	}
 }
--- Examples/src/LargestSmallest.java
+++ Examples/src/LargestSmallest.java
@@ -2,24 +2,26 @@
 public class LargestSmallest {
 
 	public static void main(String[] args) {
-		 double[] numbers={55,-452,-43,1,2,3,44,5,6,699,10,-18};
-		 double max = numbers[0];
-		 double min = numbers[0];
-		 
-		 for(int i=0; i<numbers.length; i++){
-			 if (numbers[i] > max) {
-				 max = numbers[i];
-			 	}
-			 else if (numbers[i] < min){
-				 min = numbers[i];
-			 	}
-		 	 	}
-		 
-		//double[] result = {max, min};
-		System.out.println(max);
-		System.out.println(min);
-		
-		
+		double[] numbers={5,55,-452,-43,1,2,3,44,5,6,699,10,-18};
+		double[] ret = maxMin(numbers);
+		System.out.println("Max= " + ret[0]);
+		System.out.println("Min= " + ret[1]);
 	}
-
+	
+	static double[] maxMin(double[] numbers){		
+		double max = numbers[0];
+		double min = numbers[0];
+	
+		for(int i=0; i<numbers.length; i++){
+			if (numbers[i] > max) {
+				max = numbers[i];
+			}			
+			if (numbers[i] < min){
+			 	min = numbers[i];
+			}					
+		}
+		 	
+		double[] ret = {max, min};
+		return ret;
+	}
 }
